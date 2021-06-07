217d9d606030d3db7f48828513f00716fbeee3f2
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-06-08 01:00:12 +0200

    [FIX] inv

--- src/components/profile/General.inventory.component.js
+++ src/components/profile/General.inventory.component.js
@@ -1,32 +1,42 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 export const GeneralInventory = (props) => {
 
     const INVENTORY_SPLIT_FACTOR = 4;
 
+    const [invItemsLines, setInvItemsLines] = React.useState([]);
+
 
     const renderSlots = (props) => {
         const { maxSlot, items } = props.userProfileData.inventory;
+        console.log(props.userProfileData)
 
         let invItemsLines = [];
 
         let k = [];
-        for ( let i = 0; i < maxSlot; i++) {
-            if ( items[i] ) {
-                k = [...k, items[i]];
+        for ( let i = 1; i < maxSlot; i++) {
+            if ( items[i - 1 ] ) {
+                k = [...k, items[i -1 ]];
             } else {
                 k = [...k, null];
             }
             if ( i !== 0 && i % (maxSlot / INVENTORY_SPLIT_FACTOR) === 0 ) {
                 invItemsLines = [...invItemsLines, k]
                 k = [];
-            }
+            }   
         }
-
+        setInvItemsLines(invItemsLines);
         console.log(invItemsLines);
+        return invItemsLines;
     }
+    
+    useEffect( () => {  
 
+        if ( props.userProfileData !== null ) {
+            renderSlots(props);
+        }
+    }, [props.userProfileData]) 
 
     return (
     
@@ -47,8 +57,17 @@ export const GeneralInventory = (props) => {
                         <div style={{"background": "red", "flex": "1", "margin": "5px"}}>ok</div>
                     </div>
                */}
+             {invItemsLines && invItemsLines.length && invItemsLines.map( (line,i) => {
+                        return <div className="d-flex">
+                            {
+                                line.map( item => {
+                                    return <div style={{"background": "red", "flex": "1", "margin": "5px"}}>{item === null ? "NOTHING" : item.id}</div>
 
-               {renderSlots(props)}
+                                })
+                            }
+                            </div>
+                   })
+               } 
 
            </div>
         </div>
