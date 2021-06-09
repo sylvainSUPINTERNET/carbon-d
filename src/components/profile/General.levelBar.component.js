441be3b7f276672befb7d28bea2000260610bd42
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-06-10 00:03:25 +0200

    [UPDATE] xp bar

--- src/components/profile/General.levelBar.component.js
+++ src/components/profile/General.levelBar.component.js
@@ -1,12 +1,22 @@
 import react, {useEffect, useState} from 'react';
 
 export const GeneralLevelBar = props => {    
-    const [remainsBubble, setRemainsBubble] = useState(null)
+    const [remainsBubble, setRemainsBubble] = useState(null);
+
     useEffect( () => {
         if ( props.userProfileData !== null ) {
             let arrSquare = colorXpBar(props);
             setRemainsBubble(arrSquare.filter( square => square !== 1).length)
         }
+
+        // Notification.requestPermission( (status) => {
+        //      new Notification("Time for the last battle !", {body: "🤑 notification body",
+        //       icon: "https://www.pngkey.com/png/detail/90-901844_yellow-exclamation-png-download-wow-quest-exclamation-mark.png",
+        //       image : "https://wow.zamimg.com/images/wow/icons/large/inv_sword_48.jpg"});
+        // });
+
+        // 
+
     }, [props.userProfileData]); // rerender on props change for setRemainsBuble
 
     const colorXpBar = props => {
@@ -37,7 +47,7 @@ export const GeneralLevelBar = props => {
             <div style={{flexGrow:"1"}}>
                 <h4>Niveau { props && props.userProfileData !== null && props.userProfileData.profile.level } / { props && props.userProfileData !== null && props.userProfileData.profile.maxLevel } </h4>
                
-                <p className="text-center">Barre restante : s</p>
+                <p className="text-center">Barre restante : {remainsBubble}</p>
                 <div style={{"display": "flex"}}>
                    {
                        props && props.userProfileData !== null && colorXpBar(props).map( square => {
